b67c27813705b706aae4dfaae3a0e5272c810af3
Eduardo Falcão <eduardolfalcao@gmail.com>
2019-11-19 23:09:55 -0300

    Created eclipse project, added gitignore, created transaction class

---
+++ src/br/com/edublockchain/data/Transaction.java
@@ -0,0 +1,70 @@
+package br.com.edublockchain.data;
+
+public class Transaction {
+
+	private String sender;
+	private String receiver;
+	private double amount;
+	
+	public Transaction(String sender, String receiver, double amount) {
+		this.sender = sender;
+		this.receiver = receiver;
+		this.amount = amount;
+	}
+	
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((receiver == null) ? 0 : receiver.hashCode());
+		result = prime * result + ((sender == null) ? 0 : sender.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (receiver == null) {
+			if (other.receiver != null)
+				return false;
+		} else if (!receiver.equals(other.receiver))
+			return false;
+		if (sender == null) {
+			if (other.sender != null)
+				return false;
+		} else if (!sender.equals(other.sender))
+			return false;
+		return true;
+	}
+	
+}
