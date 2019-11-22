1f9506547858880793dfb2206ca7d4c60ba7faf8
Eduardo Falcão <eduardolfalcao@gmail.com>
2019-11-22 16:22:05 -0300

    Added contract for Renting services

---
+++ RentContract.sol
@@ -0,0 +1,38 @@
+contract RentContract{
+	mapping (address => uint256) public amountPaid;
+	mapping (address => uint) public enteringTime;
+	mapping (address => uint) public stayTimeDays;
+	address renter;
+	uint dailyCost;
+
+	function RentContract(uint cost){
+		renter = msg.sender;
+		dailyCost = cost;
+	}
+
+	//called by the guest
+	function pay(uint amountOfDays) payable{
+		require(msg.value == dailyCost*amountOfDays);
+		//if true, money is already transferred to the contract
+
+		amountPaid[msg.sender] += dailyCost*amountOfDays;
+		enteringTime[msg.sender] = now;
+		stayTimeDays[msg.sender] = amountOfDays;
+	}
+
+	//called by the renter
+	function withdraw(address guest){
+		require(msg.sender==renter);
+		if((now - enteringTime[guest])/60/60/24 >= stayTimeDays[guest]){
+			msg.sender.transfer(amountPaid[guest]);
+			amountPaid[guest] = 0;
+			enteringTime[guest] = 0;
+			stayTimeDays[guest] = 0;
+		}
+	}
+	
+	//called by anyone
+	function checkPayment(address guest) returns(bool){
+		return amountPaid[guest] > 0;
+	}
+}
