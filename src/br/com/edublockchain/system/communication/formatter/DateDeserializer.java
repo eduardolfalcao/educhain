805f7473d3848fe38d816141ace014ac9f6fc990
Eduardo Falcão <eduardolfalcao@gmail.com>
2019-12-14 11:12:31 -0300

    Added Date parser for json

---
+++ src/br/com/edublockchain/system/communication/formatter/DateDeserializer.java
@@ -0,0 +1,26 @@
+package br.com.edublockchain.system.communication.formatter;
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class DateDeserializer implements JsonDeserializer<Date> {
+
+	@Override
+	public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
+		String date = element.getAsString();
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+		try {
+			return format.parse(date);
+		} catch (ParseException exp) {
+			System.err.println("Failed to parse Date:"+ exp);
+			return null;
+		}
+	}
+}
